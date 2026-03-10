c6dd54dff697284024fabed14b8f3c5934528719
mmatasic <marko.matasic@sedamit.hr>
2026-03-10 15:45:15 +0100

    i18n preparation

---
+++ public/languages.js
@@ -0,0 +1,32 @@
+// The languages array defines each dictionary that can be selected via the language picker.
+// Template for adding more entries:
+// {
+//   id: "xx",
+//   label: "Language",
+//   nativeLabel: "Native Name",
+//   code: "XX",
+//   flag: "🇽🇽",
+//   csvUrl: "assets/i18n/wingspan-dict-xx.csv",
+//   translationIndex: 2,
+// },
+
+export const languages = [
+  {
+    id: "hr",
+    label: "Croatian",
+    nativeLabel: "Hrvatski",
+    code: "HR",
+    flag: "🇭🇷",
+    csvUrl: "assets/i18n/wingspan-dict-hr.csv",
+    translationIndex: 2,
+  },
+  {
+    id: "pt",
+    label: "Portuguese",
+    nativeLabel: "Português",
+    code: "PT",
+    flag: "🇵🇹",
+    csvUrl: "assets/i18n/wingspan-dict-pt.csv",
+    translationIndex: 2,
+  },
+];
